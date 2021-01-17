b0e38de381c0e2028dbef93f32a766f0d3886179
Coki <srcn4frdm.c@gmail.com>
2021-01-17 22:14:43 +0800

    participated in keyence2021

---
+++ AtCoder/keyence2021_c2.cpp
@@ -0,0 +1,197 @@
+/**
+ * ・自力AC！
+ * ・グリッドDP
+ * ・分かれる全部の分の通り数を予め最初から持っておいて、いざ3通りに分かれる時には3/1ずつ遷移、
+ * 　みたいにやった。
+ * ・結果的に公式と同じ形になって無事ACしてるけど、この考察で合ってるんだろうか。ちょっと自信なし。。
+ * ・5000^2=2500万のグリッドDPだけど、AC1.18秒はなんか遅いなーと考えてみたら、
+ * 　最悪ケースだと毎回MOD除算が入るからlogが乗っちゃうね。。
+ * 　てことで1/3の代わりに3の逆元を前計算しておいたら、やっぱり速くなってAC0.39秒。
+ */
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define tostr to_string
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll INF = 1e18;
+constexpr ll MOD = 998244353;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+vector<ll> LIST(ll N) { vector<ll> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+pll divmod(ll a, ll b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+
+template<int mod>
+struct ModInt {
+    int x;
+
+    ModInt() : x(0) {}
+
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+    ModInt &operator+=(const ModInt &p) {
+        if((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    ModInt &operator-=(const ModInt &p) {
+        if((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    ModInt &operator*=(const ModInt &p) {
+        x = (int) (1LL * x * p.x % mod);
+        return *this;
+    }
+
+    ModInt &operator/=(const ModInt &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    ModInt operator-() const { return ModInt(-x); }
+
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+    bool operator==(const ModInt &p) const { return x == p.x; }
+
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+
+    ModInt inverse() const {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while(b > 0) {
+        t = a / b;
+        swap(a -= t * b, b);
+        swap(u -= t * v, v);
+        }
+        return ModInt(u);
+    }
+
+    ModInt pow(int64_t n) const {
+        ModInt ret(1), mul(x);
+        while(n > 0) {
+        if(n & 1) ret *= mul;
+        mul *= mul;
+        n >>= 1;
+        }
+        return ret;
+    }
+
+    friend ostream &operator<<(ostream &os, const ModInt &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, ModInt &a) {
+        int64_t t;
+        is >> t;
+        a = ModInt< mod >(t);
+        return (is);
+    }
+
+    static int get_mod() { return mod; }
+};
+using mint = ModInt<MOD>;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll H, W, K;
+    cin >> H >> W >> K;
+    auto grid = list2d(H+2, W+2, '*');
+    rep(i, 0, K) {
+        ll h, w;
+        char c;
+        cin >> h >> w >> c;
+        grid[h][w] = c;
+    }
+
+    auto dp = list2d(H+2, W+2, (mint)0);
+    dp[1][1] = pow(3LL, H*W-K, MOD);
+    mint inv3 = (mint)1 / 3;
+    rep(i, 1, H+1) {
+        rep(j, 1, W+1) {
+            if (grid[i][j] == '*') {
+                dp[i+1][j] += dp[i][j]*2*inv3;
+                dp[i][j+1] += dp[i][j]*2*inv3;
+            } elif (grid[i][j] == 'X') {
+                dp[i+1][j] += dp[i][j];
+                dp[i][j+1] += dp[i][j];
+            } elif (grid[i][j] == 'D') {
+                dp[i+1][j] += dp[i][j];
+            } else {
+                dp[i][j+1] += dp[i][j];
+            }
+        }
+    }
+    mint ans = dp[H][W];
+    print(ans);
+    return 0;
+}
