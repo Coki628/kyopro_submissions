4221d4c3c0865f08dcc8a1b13b68c3aad8a5e66a
Coki <srcn4frdm.c@gmail.com>
2021-01-20 15:16:08 +0800

    participated in cf1474

---
+++ CodeForces/cf1474_b.cpp
@@ -0,0 +1,123 @@
+/**
+ * ・なんとか自力AC
+ * ・素数列挙
+ * ・約数の間隔の狭いところをd以上にするという条件。よーくよーく考えると、
+ * 　一番狭くなるのはいつも一番最初の方なんじゃないかと思える。
+ * 　それで一定以上間隔を空けて数を並べるのを考えると、4とか6とか入れちゃうと
+ * 　2も約数になっちゃうから詰むな、と思い、素数じゃなきゃダメなのでは、となる。
+ * 　それで、1,素数a,素数b,と来ると、次はa*bになって絶対離れるので、
+ * 　これで4つ揃うし、かなりこれっぽい、となる。実際、これでACする。
+ * ・これめっちゃ悩んだんだけど、みんな爆速で通しててなんなんー、ってなった。。
+ */
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define tostr to_string
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll INF = 1e18;
+constexpr ll MOD = 1000000007;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+vector<ll> LIST(ll N) { vector<ll> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+pll divmod(ll a, ll b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+
+vector<ll> prime_list;
+
+// 素数列挙(エラトステネスの篩) 
+void eratosthenes_sieve(ll n) {
+    
+    vector<ll> table(n+1);
+    rep(i, 2, n+1) {
+        if (table[i] == 0) {
+            prime_list.pb(i);
+            for (ll j=i+i; j<=n; j+=i) {
+                table[j] = 1;
+            }
+        }
+    }
+}
+
+void solve() {
+    ll d;
+    cin >> d;
+
+    ll a = *lower_bound(ALL(prime_list), 1+d);
+    ll b = *lower_bound(ALL(prime_list), a+d);
+    ll ans = a*b;
+    print(ans);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    eratosthenes_sieve(10000000);
+
+    int T;
+    cin >> T;
+    while(T--) solve();
+
+    return 0;
+}
