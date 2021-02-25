069053e0d766f1ac05e317123fb1b4f3424698e3
Coki <srcn4frdm.c@gmail.com>
2021-02-25 15:24:16 +0800

    E8さん精選100+50問埋め：数学的な問題
    これで100問は終わり！

---
+++ AtCoder/ABC150d2.cpp
@@ -0,0 +1,356 @@
+/**
+ * ・E8さん精選100+50問埋め：数学的な問題
+ * ・めちゃくちゃ粘ってなんとか自力AC！
+ * ・LCM、周期、中国剰余定理、二分探索
+ * ・とりあえずこの半公倍数っていうものは、その数をaとするとa/2から始まって、
+ * 　aずつ増えていくという性質を持つことが分かる。
+ * ・最初雑に、多分半分にしてLCMとかだろ、って感じでやったらまあWAで通らない。
+ * ・色々試してみていくと、この半分LCMみたいなのは、数によってはどこまで行っても
+ * 　交わらないこともあるということが分かってくる。
+ * 　そういうケースを見つけ出す必要がある。さて、どうするか。
+ * ・mod xのa、みたいなものがたくさん与えられて、これら全部を考えて当てはまる数は何？
+ * 　みたいな話になるので、これは中国剰余定理が使えると思った。
+ * 　実際、これでうまく行った。(明らかにオーバーキル感のある使い方ぽいけど。。)
+ * ・これでもまだWAがあったけど、最初から無理なのが分かってるケース弾いたり
+ * 　色々頑張ったらなんとかAC。まーじで長かった…。
+ * ・にぶたんしてたとこ、さすがにただの割り算でも行けると思って式変形した。
+ */
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename T> unordered_map<T, ll> Counter(vector<T> &A) { unordered_map<T, ll> res; for (T a : A) res[a]++; return res; }
+unordered_map<char, ll> Counter(string &S) { unordered_map<char, ll> res; for (char c : S) res[c]++; return res; }
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+};
+using mint = ModInt<MOD>;
+
+////////// CRT(中国剰余定理) //////////
+// --- ACL start --- //
+#include <cassert>
+#include <numeric>
+#include <type_traits>
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+#include <utility>
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+namespace atcoder {
+namespace internal {
+constexpr long long safe_mod(long long x, long long m) {
+    x %= m;
+    if (x < 0) x += m;
+    return x;
+}
+struct barrett {
+    unsigned int _m;
+    unsigned long long im;
+    barrett(unsigned int m) : _m(m), im((unsigned long long)(-1) / m + 1) {}
+    unsigned int umod() const { return _m; }
+    unsigned int mul(unsigned int a, unsigned int b) const {
+        unsigned long long z = a;
+        z *= b;
+#ifdef _MSC_VER
+        unsigned long long x;
+        _umul128(z, im, &x);
+#else
+        unsigned long long x =
+            (unsigned long long)(((unsigned __int128)(z)*im) >> 64);
+#endif
+        unsigned int v = (unsigned int)(z - x * _m);
+        if (_m <= v) v += _m;
+        return v;
+    }
+};
+constexpr long long pow_mod_constexpr(long long x, long long n, int m) {
+    if (m == 1) return 0;
+    unsigned int _m = (unsigned int)(m);
+    unsigned long long r = 1;
+    unsigned long long y = safe_mod(x, m);
+    while (n) {
+        if (n & 1) r = (r * y) % _m;
+        y = (y * y) % _m;
+        n >>= 1;
+    }
+    return r;
+}
+constexpr bool is_prime_constexpr(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 7 || n == 61) return true;
+    if (n % 2 == 0) return false;
+    long long d = n - 1;
+    while (d % 2 == 0) d /= 2;
+    constexpr long long bases[3] = {2, 7, 61};
+    for (long long a : bases) {
+        long long t = d;
+        long long y = pow_mod_constexpr(a, t, n);
+        while (t != n - 1 && y != 1 && y != n - 1) {
+            y = y * y % n;
+            t <<= 1;
+        }
+        if (y != n - 1 && t % 2 == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+template <int n> constexpr bool is_prime = is_prime_constexpr(n);
+constexpr std::pair<long long, long long> inv_gcd(long long a, long long b) {
+    a = safe_mod(a, b);
+    if (a == 0) return {b, 0};
+    long long s = b, t = a;
+    long long m0 = 0, m1 = 1;
+    while (t) {
+        long long u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;  // |m1 * u| <= |m1| * s <= b
+        auto tmp = s;
+        s = t;
+        t = tmp;
+        tmp = m0;
+        m0 = m1;
+        m1 = tmp;
+    }
+    if (m0 < 0) m0 += b / s;
+    return {s, m0};
+}
+constexpr int primitive_root_constexpr(int m) {
+    if (m == 2) return 1;
+    if (m == 167772161) return 3;
+    if (m == 469762049) return 3;
+    if (m == 754974721) return 11;
+    if (m == 998244353) return 3;
+    int divs[20] = {};
+    divs[0] = 2;
+    int cnt = 1;
+    int x = (m - 1) / 2;
+    while (x % 2 == 0) x /= 2;
+    for (int i = 3; (long long)(i)*i <= x; i += 2) {
+        if (x % i == 0) {
+            divs[cnt++] = i;
+            while (x % i == 0) {
+                x /= i;
+            }
+        }
+    }
+    if (x > 1) {
+        divs[cnt++] = x;
+    }
+    for (int g = 2;; g++) {
+        bool ok = true;
+        for (int i = 0; i < cnt; i++) {
+            if (pow_mod_constexpr(g, (m - 1) / divs[i], m) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) return g;
+    }
+}
+template <int m> constexpr int primitive_root = primitive_root_constexpr(m);
+}  // namespace internal
+}  // namespace atcoder
+#include <algorithm>
+#include <cassert>
+#include <tuple>
+#include <vector>
+namespace atcoder {
+long long pow_mod(long long x, long long n, int m) {
+    assert(0 <= n && 1 <= m);
+    if (m == 1) return 0;
+    internal::barrett bt((unsigned int)(m));
+    unsigned int r = 1, y = (unsigned int)(internal::safe_mod(x, m));
+    while (n) {
+        if (n & 1) r = bt.mul(r, y);
+        y = bt.mul(y, y);
+        n >>= 1;
+    }
+    return r;
+}
+long long inv_mod(long long x, long long m) {
+    assert(1 <= m);
+    auto z = internal::inv_gcd(x, m);
+    assert(z.first == 1);
+    return z.second;
+}
+std::pair<long long, long long> crt(const std::vector<long long>& r,
+                                    const std::vector<long long>& m) {
+    assert(r.size() == m.size());
+    int n = int(r.size());
+    long long r0 = 0, m0 = 1;
+    for (int i = 0; i < n; i++) {
+        assert(1 <= m[i]);
+        long long r1 = internal::safe_mod(r[i], m[i]), m1 = m[i];
+        if (m0 < m1) {
+            std::swap(r0, r1);
+            std::swap(m0, m1);
+        }
+        if (m0 % m1 == 0) {
+            if (r0 % m1 != r1) return {0, 0};
+            continue;
+        }
+        long long g, im;
+        std::tie(g, im) = internal::inv_gcd(m0, m1);
+        long long u1 = (m1 / g);
+        if ((r1 - r0) % g) return {0, 0};
+        long long x = (r1 - r0) / g % u1 * im % u1;
+        r0 += x * m0;
+        m0 *= u1;  // -> lcm(m0, m1)
+        if (r0 < 0) r0 += m0;
+    }
+    return {r0, m0};
+}
+}  // namespace atcoder
+// --- ACL end --- //
+using namespace atcoder;
+
+void solve() {
+    ll N, M;
+    cin >> N >> M;
+    auto A = LIST(N);
+
+    // まず全LCMを取ってMで収まるか(後で/2するからM*2でチェック)
+    ll l = 1;
+    for (ll a : A) {
+        l = lcm(l, a);
+        if (l > M*2) {
+            print(0);
+            return;
+        }
+    }
+    // 中国剰余定理で全部が交わる場所を見つける
+    vector<ll> R(N), mo(N);
+    rep(i, N) {
+        // (a/2) mod a の形を全部集める
+        R[i] = A[i]/2;
+        mo[i] = A[i];
+    }
+    auto res = crt(R, mo);
+    // 解なし or あるけど最初からM超 ならNG
+    if (res.first == 0 and res.second == 0 or res.first > M) {
+        print(0);
+        return;
+    }
+    ll m = res.second;
+    ll base = res.first;
+    // 周期何回行けるか
+    // ll ret = bisearch_max(0, INF, [&](ll x) {
+    //     if (base+m*(ld)x > INF) return false; 
+    //     return base+m*x <= M;
+    // });
+    ll ret = (M-base)/m;    
+    // baseの分足す
+    ret++;
+    print(ret);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    solve();
+
+    return 0;
+}
