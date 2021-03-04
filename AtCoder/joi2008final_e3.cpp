611183c3f848784ad58ca4386b696a32d77197ea
Coki <srcn4frdm.c@gmail.com>
2021-03-04 23:23:51 +0800

    E8さん精選100+50問埋め：座標圧縮

---
+++ AtCoder/joi2008final_e3.cpp
@@ -0,0 +1,221 @@
+/**
+ * ・E8さん精選100+50問埋め：座標圧縮
+ * ・きっちり自力AC！
+ * ・座標圧縮、2次元いもす、グリッドBFS
+ * ・やる。座圧は半開区間を意識すればバグらない。BFSする時には一番外側をマスとして使わないように、1つ小さくやる。
+ */
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
+constexpr ld EPS = 1e-10;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename T> unordered_map<T, ll> Counter(vector<T> &A) { unordered_map<T, ll> res; for (T a : A) res[a]++; return res; }
+unordered_map<char, ll> Counter(string &S) { unordered_map<char, ll> res; for (char c : S) res[c]++; return res; }
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+};
+using mint = ModInt<MOD>;
+
+using mli = unordered_map<ll, int>;
+using mil = unordered_map<int, ll>;
+pair<mli, mil> compress(vector<ll> A) {
+    mli zipped;
+    mil unzipped;
+    sort(A.begin(), A.end());
+    A.erase(unique(A.begin(), A.end()), A.end());
+    rep(i, 0, A.size()) {
+        zipped[A[i]] = i;
+        unzipped[i] = A[i];
+    }
+    return {zipped, unzipped};
+}
+
+void bfs(vvl &grid, vector<vector<bool>> &visited, vector<pii> src) {
+
+    int H = grid.size();
+    int W = grid[0].size();
+    const vector<pii> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    queue<pii> que;
+    for (auto [h, w] : src) {
+        que.push({h, w});
+        visited[h][w] = 1;
+    }
+    while (!que.empty()) {
+        auto [h, w] = que.front(); que.pop();
+        for (auto [dh, dw] : directions) {
+            int nh = h + dh;
+            int nw = w + dw;
+            if (nh < 0 or nw < 0 or nh >= H-1 or nw >= W-1) continue;
+            if (grid[nh][nw] != 0) {
+                continue;
+            }
+            if (!visited[nh][nw]) {
+                visited[nh][nw] = 1;
+                que.push({nh, nw});
+            }
+        }
+    }
+}
+
+void solve() {
+    ll W, H;
+    cin >> W >> H;
+    ll N;
+    cin >> N;
+    vector<ll> X1(N), Y1(N), X2(N), Y2(N);
+    vector<ll> S1, S2;
+    rep(i, N) {
+        cin >> X1[i] >> Y1[i] >> X2[i] >> Y2[i];
+        S1.pb(X1[i]);
+        S1.pb(X2[i]);
+        S2.pb(Y1[i]);
+        S2.pb(Y2[i]);
+    }
+    S1.pb(0);
+    S2.pb(0);
+    S1.pb(W);
+    S2.pb(H);
+
+    auto [zpx, unzpx] = compress(S1);
+    auto [zpy, unzpy] = compress(S2);
+    ll M1 = zpx.size();
+    ll M2 = zpy.size();
+
+    auto imos = list2d(M2, M1, 0LL);
+    rep(i, N) {
+        imos[zpy[Y1[i]]][zpx[X1[i]]]++;
+        imos[zpy[Y2[i]]][zpx[X2[i]]]++;
+        imos[zpy[Y1[i]]][zpx[X2[i]]]--;
+        imos[zpy[Y2[i]]][zpx[X1[i]]]--;
+    }
+    rep(i, M2) {
+        rep(j, M1-1) {
+            imos[i][j+1] += imos[i][j];
+        }
+    }
+    rep(j, M1) {
+        rep(i, M2-1) {
+            imos[i+1][j] += imos[i][j];
+        }
+    }
+
+    auto visited = list2d(M2, M1, false);
+    ll ans = 0;
+    rep(i, M2-1) {
+        rep(j, M1-1) {
+            if (!imos[i][j] and !visited[i][j]) {
+                ans++;
+                bfs(imos, visited, {{i, j}});
+            }
+        }
+    }
+    print(ans);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    solve();
+
+    return 0;
+}
