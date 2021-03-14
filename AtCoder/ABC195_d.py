88a46385555916c6c8895cf0a77f5d2d4f0dd148
Coki <srcn4frdm.c@gmail.com>
2021-03-15 00:39:25 +0800

    participated in ABC195

---
+++ AtCoder/ABC195_d.py
@@ -0,0 +1,135 @@
+"""
+・なんとか自力AC！
+・最小費用流(ベルマンフォード版)、重み付き2部マッチング
+・ACLの最小費用流ちゃんと動かしてなかったせいで時間かかった。
+　結局負コストそのまま乗せれるこっちで通した。
+　1ずつ流して、流せなくなるまで利得を取れるだけ取るような形にしてる。
+"""
+
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def list2d(a, b, c): return [[c] * b for i in range(a)]
+def list3d(a, b, c, d): return [[[d] * c for j in range(b)] for i in range(a)]
+def list4d(a, b, c, d, e): return [[[[e] * d for k in range(c)] for j in range(b)] for i in range(a)]
+def ceil(x, y=1): return int(-(-x // y))
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(N=None): return list(MAP()) if N is None else [INT() for i in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+sys.setrecursionlimit(10**6)
+INF = 10**18
+MOD = 10**9 + 7
+EPS = 10**-10
+
+class MinCostFlow:
+    """ 最小費用流(ベルマンフォード版、負コストに対応可)：O(F*E*V) """
+
+    INF = 10**18
+
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+        self.pos = []
+    
+    def copy(self):
+        res = MinCostFlow(self.N)
+        res.G = [[a[:] for a in b] for b in self.G]
+        res.pos = [a[:] for a in self.pos]
+        return res
+
+    def add_edge(self, fr, to, cap, cost):
+        self.pos.append((fr, len(self.G[fr])))
+        self.G[fr].append([to, cap, cost, len(self.G[to])])
+        self.G[to].append([fr, 0, -cost, len(self.G[fr])-1])
+
+    def flow(self, s, t, f):
+
+        N = self.N; G = self.G
+        INF = MinCostFlow.INF
+
+        res = 0
+        prv_v = [0] * N
+        prv_e = [0] * N
+
+        while f:
+            dist = [INF] * N
+            dist[s] = 0
+            update = True
+
+            while update:
+                update = False
+                for v in range(N):
+                    if dist[v] == INF:
+                        continue
+                    for i, (to, cap, cost, _) in enumerate(G[v]):
+                        if cap > 0 and dist[to] > dist[v] + cost:
+                            dist[to] = dist[v] + cost
+                            prv_v[to] = v; prv_e[to] = i
+                            update = True
+            if dist[t] == INF:
+                return INF
+
+            d = f; v = t
+            while v != s:
+                d = min(d, G[prv_v[v]][prv_e[v]][1])
+                v = prv_v[v]
+            f -= d
+            res += d * dist[t]
+            v = t
+            while v != s:
+                e = G[prv_v[v]][prv_e[v]]
+                e[1] -= d
+                G[v][e[3]][1] += d
+                v = prv_v[v]
+        return res
+
+    def get_edge(self, i):
+        e = self.G[self.pos[i][0]][self.pos[i][1]]
+        re = self.G[e[0]][e[3]]
+        # from, to, cap, flow, cost
+        return (self.pos[i][0], e[0], e[1]+re[1], re[1], e[2])
+
+    def get_edges(self):
+        M = len(self.pos)
+        res = []
+        for i in range(M):
+            res.append(self.get_edge(i))
+        return res
+
+N, M, Q = MAP()
+WV = []
+for i in range(N):
+    w, v = MAP()
+    WV.append((w, v))
+A = LIST()
+
+for _ in range(Q):
+    l, r = MAP()
+    l -= 1
+
+    mcf = MinCostFlow(N+M+2)
+    s = N+M
+    t = N+M+1
+    for i in range(N):
+        mcf.add_edge(s, i, 1, 0)
+        w, v = WV[i]
+        for j in range(M):
+            if l <= j < r:
+                continue
+            if w <= A[j]:
+                mcf.add_edge(i, N+j, 1, -v)
+    for i in range(M):
+        mcf.add_edge(N+i, t, 1, 0)
+    
+    ans = 0
+    for i in range(1, N+1):
+        res = -mcf.flow(s, t, 1)
+        if res != -INF:
+            ans += res
+        else:
+            break
+    print(ans)
---
+++ AtCoder/ABC195_f.cpp
@@ -0,0 +1,193 @@
+/**
+ * 参考：https://blog.hamayanhamayan.com/entry/2021/03/14/002322
+ * ・自力ならず。
+ * ・素数列挙、bitDP応用
+ * ・どうにか素因数とかに絞り込んで状態減らしたbitDPだろう、
+ * 　とは思ってたものの、正解に辿り着く詰め方はできなかった。
+ * ・公式解説とかTLで、必要な素因数がB-Aの72まででいい理由が釈然としなかったんだけど、
+ * 　はまやんさんの説明から、値の差が最大で72しかないから、73以上ある素因数だと
+ * 　AからBの範囲内の値にそれを掛けた時に、絶対Bより大きくなってしまう。
+ * 　よって考慮しなくていい。ってのが一番しっくり来た。
+ * ・これで考慮しないといけない素数が20個だけになったので、bitDPで状態が持てる。
+ * 　ある値A[i]を使うor使わないの遷移を、ナップザックみたく進めていく。
+ * 　この時、使う遷移は今の状態と重ねた時素因数が被らない場合のみできる。
+ */
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr ld EPS = 1e-10;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename T> unordered_map<T, ll> Counter(vector<T> &A) { unordered_map<T, ll> res; for (T a : A) res[a]++; return res; }
+unordered_map<char, ll> Counter(string &S) { unordered_map<char, ll> res; for (char c : S) res[c]++; return res; }
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+    // operator int() const { return x; }
+};
+using mint = ModInt<MOD>;
+
+vector<ll> eratosthenes_sieve(ll n) {
+
+    vector<bool> table(n+1);
+    vector<ll> primes;
+    rep(i, 2, n+1) {
+        if (table[i] == 0) {
+            primes.pb(i);
+            for (ll j=i+i; j<=n; j+=i) {
+                table[j] = 1;
+            }
+        }
+    }
+    return primes;
+}
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+
+    vector<ll> A;
+    rep(i, a, b+1) A.pb(i);
+    ll N = A.size();
+    const ll MX = 72;
+
+    auto primes = eratosthenes_sieve(MX);
+    ll M = primes.size();
+    vector<ll> B(N);
+    rep(i, N) {
+        rep(j, M) {
+            // A[i]がこの素因数を持つならビットを立てる
+            if (A[i]%primes[j] == 0) {
+                B[i] |= 1<<j;
+            }
+        }
+    }
+
+    auto dp = list2d(N+1, 1<<M, 0LL);
+    dp[0][0] = 1;
+    rep(i, N) {
+        rep(bit, 1<<M) {
+            // A[i]を使わない遷移
+            dp[i+1][bit] += dp[i][bit];
+            // 重複する素因数がなかったら、A[i]を使う遷移
+            if ((bit&B[i]) == 0) {
+                dp[i+1][bit|B[i]] += dp[i][bit];
+            }
+        }
+    }
+    ll ans = sum(dp[N]);
+    print(ans);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
