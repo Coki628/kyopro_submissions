6fd21b8675b184b09f6075dcb7151d89ed1bed3f
Coki <srcn4frdm.c@gmail.com>
2021-04-03 02:55:43 +0800

    E8さん精選100+50問埋め：BIT、RMQ

---
+++ AOJ/aoj0365_pck2017prelim_2.cpp
@@ -0,0 +1,401 @@
+/*
+・E8さん精選100+50問埋め：Binary Indexed Tree (BIT)
+・なんとか自力AC！だいぶ時間かかったけど。。
+・文字列で転倒数、遅延セグ木、区間最小値取得・区間加算、セグ木にぶたん
+・絶対想定解じゃなさそうだけど通しきった。
+・まず各位置毎に転倒数を取っておいて、この中から使えるやつを貪欲に取り、
+　その時に変化する転倒数を更新する方針を考える。
+・{文字, 転倒数}ペアにして文字順でソートすれば、前から辞書順が並ぶ。
+　転倒数の方を遅延セグ木に乗せて、値がK以下の最小位置を常に取り続ける。
+　これはセグ木にぶたんでできる。
+　ある位置の文字を選んだ時、その文字より辞書順が早い文字は、
+　自分より辞書順の遅い文字に抜かされた分転倒数が1減るので、
+　これはセグ木上で、取った位置未満の位置を区間加算で-1するとできる。
+　この時、使った位置は今後使わないように適当に大きい数INFとか入れておく。
+　ここをMODにしてたら小さすぎてWAだった。。
+　INFを加算して壊れないか心配だったけど、各位置1回だけだしまあ大丈夫みたい。
+・20万のNに遅延セグ木にぶたんとか重そうー、って思ったけど、問題なくAC0.4秒。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+#define tostr to_string
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr ld EPS = 1e-10;
+constexpr ld PI = M_PI;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+int bit_length(ll x) { return floor(log2(x))+1; }
+string bin(ll x) { string res; while (x) { if (x & 1) res += '1'; else res += '0'; x >>= 1; } reverse(ALL(res)); if(res == "") res += '0'; return res; }
+
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename T> unordered_map<T, ll> Counter(vector<T> &A) { unordered_map<T, ll> res; for (T a : A) res[a]++; return res; }
+unordered_map<char, ll> Counter(string &S) { unordered_map<char, ll> res; for (char c : S) res[c]++; return res; }
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    bool operator<(const ModInt &p) const { return x < p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+    // operator int() const { return x; }
+};
+using mint = ModInt<MOD>;
+
+template<typename T>
+struct BIT {
+
+    int sz;
+    vector<T> tree;
+
+    BIT(int n) {
+        // 0-indexed
+        n++;
+        sz = 1;
+        while (sz < n) {
+            sz *= 2;
+        }
+        tree.resize(sz);
+    }
+
+    // [0, i]を合計する
+    T sum(int i) {
+        T s = 0;
+        i++;
+        while (i > 0) {
+            s += tree[i-1];
+            i -= i & -i;
+        }
+        return s;
+    }
+
+    void add(int i, T x) {
+        i++;
+        while (i <= sz) {
+            tree[i-1] += x;
+            i += i & -i;
+        }
+    }
+
+    // 区間和の取得 [l, r)
+    T query(int l, int r) {
+        return sum(r-1) - sum(l-1);
+    }
+
+    T get(int i) {
+        return query(i, i+1);
+    }
+
+    void update(int i, T x) {
+        add(i, x - get(i));
+    }
+
+    T operator[](int i) {
+        return get(i);
+    }
+};
+
+template<typename T, typename E>
+struct LazySegTree {
+    using F = function<T(T, T)>;
+    using G = function<T(T, E)>;
+    using H = function<E(E, E)>;
+    int n, height;
+    F f;
+    G g;
+    H h;
+    T ti;
+    E ei;
+    vector<T> dat;
+    vector<E> laz;
+    LazySegTree(F f, G g, H h, T ti, E ei):
+        f(f), g(g), h(h), ti(ti), ei(ei) {}
+
+    void init(int n_){
+        n = 1; height = 0;
+        while (n<n_) n<<=1, height++;
+        dat.assign(2*n, ti);
+        laz.assign(2*n, ei);
+    }
+
+    void build(int n_) {
+        init(n_);
+    }
+
+    void build(const vector<T> &v){
+        int n_ = v.size();
+        init(n_);
+        for (int i=0;i<n_;i++) dat[n+i] = v[i];
+        for (int i=n-1;i;i--)
+        dat[i] = f(dat[(i<<1)|0], dat[(i<<1)|1]);
+    }
+
+    inline T reflect(int k){
+        return laz[k]==ei?dat[k]:g(dat[k], laz[k]);
+    }
+
+    inline void propagate(int k){
+        if (laz[k]==ei) return;
+        laz[(k<<1)|0] = h(laz[(k<<1)|0], laz[k]);
+        laz[(k<<1)|1] = h(laz[(k<<1)|1], laz[k]);
+        dat[k] = reflect(k);
+        laz[k] = ei;
+    }
+
+    inline void thrust(int k){
+        for (int i=height;i;i--) propagate(k>>i);
+    }
+
+    inline void recalc(int k){
+        while (k>>=1)
+        dat[k] = f(reflect((k<<1)|0), reflect((k<<1)|1));
+    }
+
+    void update(int a, int b, E x){
+        if (a>=b) return;
+        thrust(a+=n);
+        thrust(b+=n-1);
+        for (int l=a,r=b+1;l<r;l>>=1,r>>=1) {
+            if (l&1) laz[l] = h(laz[l], x), l++;
+            if (r&1) --r, laz[r] = h(laz[r], x);
+        }
+        recalc(a);
+        recalc(b);
+    }
+
+    void set_val(int a, T x){
+        thrust(a+=n);
+        dat[a] = x; laz[a] = ei;
+        recalc(a);
+    }
+
+    T query(int a, int b){
+        if (a>=b) return ti;
+        thrust(a+=n);
+        thrust(b+=n-1);
+        T vl = ti, vr = ti;
+        for (int l=a,r=b+1;l<r;l>>=1,r>>=1) {
+            if (l&1) vl = f(vl, reflect(l++));
+            if (r&1) vr = f(reflect(--r), vr);
+        }
+        return f(vl, vr);
+    }
+
+    template<typename C>
+    int find(int st, C &check, T &acc, int k, int l, int r){
+        if (l+1==r) {
+            acc = f(acc, reflect(k));
+            return check(acc)?k-n:-1;
+        }
+        propagate(k);
+        int m = (l+r)>>1;
+        if (m<=st) return find(st, check, acc, (k<<1)|1, m, r);
+        if (st<=l&&!check(f(acc,dat[k]))) {
+            acc = f(acc,dat[k]);
+            return -1;
+        }
+        int vl = find(st, check, acc, (k<<1)|0, l, m);
+        if (~vl) return vl;
+        return find(st, check, acc, (k<<1)|1, m, r);
+    }
+
+    template<typename C>
+    int find(int st, C &check){
+        T acc = ti;
+        return find(st, check, acc, 1, 0, n);
+    }
+
+    void update(int i, E x) {
+        update(i, i+1, x);
+    }
+
+    T operator[](int i) {
+        return query(i, i+1);
+    }
+
+    void print(int n) {
+        rep(i, 0, n) {
+            cout << query(i, i+1);
+            if (i == n-1) cout << endl;
+            else cout << ' ';
+        }
+    }
+
+    // 区間[l,r]で左から最初にxに対して比較の条件を満たすような値が出現する位置
+    template<typename G> ll bisearch_fore(ll l, ll r, T x, const G &compare) {
+        ll ok = r + 1;
+        ll ng = l - 1;
+        while (ng+1 < ok) {
+            ll mid = (ok+ng) / 2;
+            if (compare(query(l, mid+1), x)) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+        if (ok != r + 1) {
+            return ok;
+        } else {
+            return -1;
+        }
+    }
+};
+
+// 区間最小値取得・区間加算
+auto f = [](ll a, ll b) -> ll { return min(a, b); };
+auto g = [](ll a, ll b) -> ll { return a + b; };
+auto h = [](ll a, ll b) -> ll { return a + b; };
+
+template<typename T1, typename T2>
+pair<vector<T1>, vector<T2>> zip(vector<pair<T1, T2>> &A) {
+    ll N = A.size();
+    pair<vector<T1>, vector<T2>> res = {vector<T1>(N), vector<T2>(N)};
+    rep(i, N) {
+        res.first[i] = A[i].first;
+        res.second[i] = A[i].second;
+    }
+    return res;
+}
+
+void solve() {
+    string S;
+    cin >> S;
+    ll N = S.size();
+    ll K;
+    cin >> K;
+
+    vector<ll> A(N);
+    rep(i, N) A[i] = S[i]-'a';
+
+    const ll M = 26;
+    BIT<ll> bit(M);
+    vector<pair<ll, ll>> inv(N);
+    rep(i, N) {
+        inv[i] = {A[i], bit.query(A[i]+1, M)};
+        bit.add(A[i], 1);
+    }
+    sort(ALL(inv));
+    auto [A2, inv2] = zip(inv);
+    LazySegTree<ll, ll> seg(f, g, h, INF, 0);
+    seg.build(inv2);
+
+    string ans;
+    rep(_, N) {
+        ll i = seg.bisearch_fore(0, N-1, K, less_equal<ll>());
+        ans += A2[i]+'a';
+        K -= seg[i];
+        seg.update(i, INF);
+        seg.update(0, i, -1);
+    }
+    print(ans);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
