3e9688392f8ff3bd63fe2d3c5100a9b9a57ecc3f
Coki <srcn4frdm.c@gmail.com>
2021-05-24 00:10:06 +0900

    participated in ABC202

---
+++ AtCoder/ABC202_e.cpp
@@ -0,0 +1,449 @@
+/*
+・終了後自力AC！でもこれ通したかった。。
+・HL分解、部分木クエリ、Wavelet Matrix
+・HLDで部分木クエリすれば目的の範囲は取れる。
+　深さDちょうどはHLD番号で振り直した深さをWavelet Matrixに乗せて、
+　range_freqした。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+#define tostr to_string
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr ld EPS = 1e-10;
+constexpr ld PI = M_PI;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+#define debug(x) (cout << #x << ": ", print(x));
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+int bit_length(ll x) { return x != 0 ? floor(log2(x))+1 : 0; }
+string bin(ll x) { string res; while (x) { if (x & 1) res += '1'; else res += '0'; x >>= 1; } reverse(ALL(res)); if(res == "") res += '0'; return res; }
+
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+ld degrees(ld radians) { return radians * 180.0 / PI; }
+ld radians(ld degrees) { return degrees * PI / 180.0; }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+
+template<typename T> map<T, ll> Counter(vector<T> &A) { map<T, ll> res; for (T a : A) res[a]++; return res; }
+map<char, ll> Counter(string &S) { map<char, ll> res; for (char c : S) res[c]++; return res; }
+template<typename T1, typename T2> pair<vector<T1>, vector<T2>> zip(vector<pair<T1, T2>> &A) { ll N = A.size(); pair<vector<T1>, vector<T2>> res = {vector<T1>(N), vector<T2>(N)}; rep(i, N) { res.first[i] = A[i].first; res.second[i] = A[i].second; } return res; }
+
+template<typename T> struct Accumulate {
+    vector<T> acc; int N;
+    Accumulate(vector<T> &A) { N = A.size(); acc = A; rep(i, N-1) acc[i+1] += acc[i]; acc.insert(acc.begin(), 0); }
+    T query(int l, int r) { assert(0 <= l and l <= N and 0 <= r and r <= N); return acc[r]-acc[l]; }
+    T get(int i) { return query(i, i+1); }
+    T operator[](int i) { return query(i, i+1); }
+};
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    bool operator<(const ModInt &p) const { return x < p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+    // operator int() const { return x; }
+};
+using mint = ModInt<MOD>;
+
+// HD分解
+struct HeavyLightDecomposition {
+public:
+    vvi &g;
+    vector< int > sz, in, out, head, rev, par, dep;
+
+    void build() {
+        sz.assign(g.size(), 0);
+        in.assign(g.size(), 0);
+        out.assign(g.size(), 0);
+        head.assign(g.size(), 0);
+        rev.assign(g.size(), 0);
+        par.assign(g.size(), 0);
+        dep.assign(g.size(), 0);
+        dfs_sz(0, -1, 0);
+        int t = 0;
+        dfs_hld(0, -1, t);
+    }
+
+    /* k: 0-indexed */
+    int la(int v, int k) {
+        while(1) {
+            int u = head[v];
+            if(in[v] - k >= in[u]) return rev[in[v] - k];
+            k -= in[v] - in[u] + 1;
+            v = par[u];
+        }
+    }
+
+    int lca(int u, int v) const {
+        for(;; v = par[head[v]]) {
+            if(in[u] > in[v]) swap(u, v);
+            if(head[u] == head[v]) return u;
+        }
+    }
+
+    int dist(int u, int v) const {
+        return dep[u] + dep[v] - 2 * dep[lca(u, v)];
+    }
+
+    template< typename E, typename Q, typename F, typename S >
+    E query(int u, int v, const E &ti, const Q &q, const F &f, const S &s, bool edge = false) {
+        E l = ti, r = ti;
+        for(;; v = par[head[v]]) {
+            if(in[u] > in[v]) swap(u, v), swap(l, r);
+            if(head[u] == head[v]) break;
+            l = f(q(in[head[v]], in[v] + 1), l);
+        }
+        return s(f(q(in[u] + edge, in[v] + 1), l), r);
+    }
+
+    template< typename E, typename Q, typename F >
+    E query(int u, int v, const E &ti, const Q &q, const F &f, bool edge = false) {
+        return query(u, v, ti, q, f, f, edge);
+    }
+
+    template< typename Q >
+    void add(int u, int v, const Q &q, bool edge = false) {
+        for(;; v = par[head[v]]) {
+            if(in[u] > in[v]) swap(u, v);
+            if(head[u] == head[v]) break;
+            q(in[head[v]], in[v] + 1);
+        }
+        q(in[u] + edge, in[v] + 1);
+    }
+
+    /* {parent, child} */
+    vector< pair< int, int > > compress(vector< int > &remark) {
+        auto cmp = [&](int a, int b) { return in[a] < in[b]; };
+        sort(begin(remark), end(remark), cmp);
+        remark.erase(unique(begin(remark), end(remark)), end(remark));
+        int K = (int) remark.size();
+        for(int k = 1; k < K; k++) remark.emplace_back(lca(remark[k - 1], remark[k]));
+        sort(begin(remark), end(remark), cmp);
+        remark.erase(unique(begin(remark), end(remark)), end(remark));
+        vector< pair< int, int > > es;
+        stack< int > st;
+        for(auto &k : remark) {
+            while(!st.empty() && out[st.top()] <= in[k]) st.pop();
+            if(!st.empty()) es.emplace_back(st.top(), k);
+            st.emplace(k);
+        }
+        return es;
+    }
+
+    explicit HeavyLightDecomposition(vvi &g) : g(g) {}
+
+    private:
+    void dfs_sz(int idx, int p, int d) {
+        dep[idx] = d;
+        par[idx] = p;
+        sz[idx] = 1;
+        if(g[idx].size() && g[idx][0] == p) swap(g[idx][0], g[idx].back());
+        for(auto &to : g[idx]) {
+            if(to == p) continue;
+            dfs_sz(to, idx, d + 1);
+            sz[idx] += sz[to];
+            if(sz[g[idx][0]] < sz[to]) swap(g[idx][0], to);
+        }
+    }
+
+    void dfs_hld(int idx, int p, int &times) {
+        in[idx] = times++;
+        rev[in[idx]] = idx;
+        for(auto &to : g[idx]) {
+            if(to == p) continue;
+            head[to] = (g[idx][0] == to ? head[idx] : to);
+            dfs_hld(to, idx, times);
+        }
+        out[idx] = times;
+    }
+};
+
+
+// 完備辞書(Wavelet Matrixとセットで使う)
+struct SuccinctIndexableDictionary {
+    size_t length;
+    size_t blocks;
+    vector< unsigned > bit, sum;
+
+    SuccinctIndexableDictionary() = default;
+
+    SuccinctIndexableDictionary(size_t length) : length(length), blocks((length + 31) >> 5) {
+        bit.assign(blocks, 0U);
+        sum.assign(blocks, 0U);
+    }
+
+    void set(int k) {
+        bit[k >> 5] |= 1U << (k & 31);
+    }
+
+    void build() {
+        sum[0] = 0U;
+        for(int i = 1; i < blocks; i++) {
+        sum[i] = sum[i - 1] + __builtin_popcount(bit[i - 1]);
+        }
+    }
+
+    bool operator[](int k) {
+        return (bool((bit[k >> 5] >> (k & 31)) & 1));
+    }
+
+    int rank(int k) {
+        return (sum[k >> 5] + __builtin_popcount(bit[k >> 5] & ((1U << (k & 31)) - 1)));
+    }
+
+    int rank(bool val, int k) {
+        return (val ? rank(k) : k - rank(k));
+    }
+};
+
+// Wavelet Matrix
+template< typename T, int MAXLOG >
+struct WaveletMatrix {
+    size_t length;
+    SuccinctIndexableDictionary matrix[MAXLOG];
+    int mid[MAXLOG];
+
+    WaveletMatrix() = default;
+
+    WaveletMatrix(vector< T > v) : length(v.size()) {
+        vector< T > l(length), r(length);
+        for(int level = MAXLOG - 1; level >= 0; level--) {
+        matrix[level] = SuccinctIndexableDictionary(length + 1);
+        int left = 0, right = 0;
+        for(int i = 0; i < length; i++) {
+            if(((v[i] >> level) & 1)) {
+            matrix[level].set(i);
+            r[right++] = v[i];
+            } else {
+            l[left++] = v[i];
+            }
+        }
+        mid[level] = left;
+        matrix[level].build();
+        v.swap(l);
+        for(int i = 0; i < right; i++) {
+            v[left + i] = r[i];
+        }
+        }
+    }
+
+    pair< int, int > succ(bool f, int l, int r, int level) {
+        return {matrix[level].rank(f, l) + mid[level] * f, matrix[level].rank(f, r) + mid[level] * f};
+    }
+
+    // v[k]
+    T access(int k) {
+        T ret = 0;
+        for(int level = MAXLOG - 1; level >= 0; level--) {
+        bool f = matrix[level][k];
+        if(f) ret |= T(1) << level;
+        k = matrix[level].rank(f, k) + mid[level] * f;
+        }
+        return ret;
+    }
+
+    T operator[](const int &k) {
+        return access(k);
+    }
+
+    // count i s.t. (0 <= i < r) && v[i] == x
+    int rank(const T &x, int r) {
+        int l = 0;
+        for(int level = MAXLOG - 1; level >= 0; level--) {
+        tie(l, r) = succ((x >> level) & 1, l, r, level);
+        }
+        return r - l;
+    }
+
+    // k-th(0-indexed) smallest number in v[l,r)
+    T kth_smallest(int l, int r, int k) {
+        assert(0 <= k && k < r - l);
+        T ret = 0;
+        for(int level = MAXLOG - 1; level >= 0; level--) {
+        int cnt = matrix[level].rank(false, r) - matrix[level].rank(false, l);
+        bool f = cnt <= k;
+        if(f) {
+            ret |= T(1) << level;
+            k -= cnt;
+        }
+        tie(l, r) = succ(f, l, r, level);
+        }
+        return ret;
+    }
+
+    // k-th(0-indexed) largest number in v[l,r)
+    T kth_largest(int l, int r, int k) {
+        return kth_smallest(l, r, r - l - k - 1);
+    }
+
+    // count i s.t. (l <= i < r) && (v[i] < upper)
+    int range_freq(int l, int r, T upper) {
+        int ret = 0;
+        for(int level = MAXLOG - 1; level >= 0; level--) {
+        bool f = ((upper >> level) & 1);
+        if(f) ret += matrix[level].rank(false, r) - matrix[level].rank(false, l);
+        tie(l, r) = succ(f, l, r, level);
+        }
+        return ret;
+    }
+
+    // count i s.t. (l <= i < r) && (lower <= v[i] < upper)
+    int range_freq(int l, int r, T lower, T upper) {
+        return range_freq(l, r, upper) - range_freq(l, r, lower);
+    }
+
+    // max v[i] s.t. (l <= i < r) && (v[i] < upper)
+    T prev_value(int l, int r, T upper) {
+        int cnt = range_freq(l, r, upper);
+        return cnt == 0 ? T(-1) : kth_smallest(l, r, cnt - 1);
+    }
+
+    // min v[i] s.t. (l <= i < r) && (lower <= v[i])
+    T next_value(int l, int r, T lower) {
+        int cnt = range_freq(l, r, lower);
+        return cnt == r - l ? T(-1) : kth_smallest(l, r, cnt);
+    }
+};
+
+void solve() {
+    ll N;
+    cin >> N;
+    vvi nodes(N);
+    rep(i, 1, N) {
+        ll p;
+        cin >> p;
+        p--;
+        nodes[i].pb(p);
+        nodes[p].pb(i);
+    }
+
+    HeavyLightDecomposition hld(nodes);
+    hld.build();
+    // 深さをHLD番号にエンコード
+    vector<int> depth(N);
+    rep(i, N) depth[hld.in[i]] = hld.dep[i];
+    WaveletMatrix<int, 18> wm(depth);
+
+    ll Q;
+    cin >> Q;
+    rep(_, Q) {
+        ll u, d;
+        cin >> u >> d;
+        u--;
+        ll l = hld.in[u];
+        ll r = hld.out[u];
+        auto res = wm.range_freq(l, r, d, d+1);
+        print(res);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
