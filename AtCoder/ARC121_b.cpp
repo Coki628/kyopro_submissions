f76deb504249e51e8348176e46ee0216428454ec
Coki <srcn4frdm.c@gmail.com>
2021-06-01 01:32:41 +0900

    participated in ARC121

---
+++ AtCoder/ARC121_b.cpp
@@ -0,0 +1,447 @@
+/*
+参考：https://twitter.com/kawara_y_kyopro/status/1398645449486077962
+　　　https://atcoder.jp/contests/arc121/submissions/22996091
+・自力ならず。。
+・重み付きマッチング、最小費用流
+・かわらさんがツイートでフローで通したって言ってたからそれで。
+・自分も「GB,BR型の組が1つずつあり、他は全て同色の色」のパターンの実装で詰まった。
+　同じBを取り合っちゃう可能性をうまく排除する方法が見つからず。。
+　フローちょっと考えたけど、頂点20万だし辺も多いし無理だろ、
+　って思ったらいけるんだね。。
+・辺は全ペアに張ったらそれは無理だろうけど、今回は流量2しか流さないので、
+　2つが競合しなければどこでもいい訳で、そんな割の悪いところは繋ぐ必要がない。
+　とりあえず適当に、にぶたんした前後3つずつだけつないだけど、これで通った。
+　それでも辺は数10万はあるだろうけど、AC0.24秒。
+　流量も少ないから速いのかな。いずれにせよ制約10万級で最小フローが流せるなら、
+　かなり活用の幅はありそうだよね。
+・あと、自分でフローのグラフ組んだら何かWAになって結局かわらさんのコード見たんだけど、
+　中継の頂点作ってなかったから、片側から2つとも流れちゃうパターンができてたっぽい。
+　フローの経験値もっと積まないとな。。。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+#define tostr to_string
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr ld EPS = 1e-10;
+constexpr ld PI = M_PI;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+#define debug(x) (cout << #x << ": ", print(x));
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+int bit_length(ll x) { return x != 0 ? floor(log2(x))+1 : 0; }
+string bin(ll x) { string res; while (x) { if (x & 1) res += '1'; else res += '0'; x >>= 1; } reverse(ALL(res)); if(res == "") res += '0'; return res; }
+
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+ld degrees(ld radians) { return radians * 180.0 / PI; }
+ld radians(ld degrees) { return degrees * PI / 180.0; }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+
+template<typename T> map<T, ll> Counter(vector<T> &A) { map<T, ll> res; for (T a : A) res[a]++; return res; }
+map<char, ll> Counter(string &S) { map<char, ll> res; for (char c : S) res[c]++; return res; }
+template<typename T1, typename T2> pair<vector<T1>, vector<T2>> zip(vector<pair<T1, T2>> &A) { ll N = A.size(); pair<vector<T1>, vector<T2>> res = {vector<T1>(N), vector<T2>(N)}; rep(i, N) { res.first[i] = A[i].first; res.second[i] = A[i].second; } return res; }
+
+template<typename T> struct Accumulate {
+    vector<T> acc; int N;
+    Accumulate(vector<T> &A) { N = A.size(); acc = A; rep(i, N-1) acc[i+1] += acc[i]; acc.insert(acc.begin(), 0); }
+    T query(int l, int r) { assert(0 <= l and l <= N and 0 <= r and r <= N); return acc[r]-acc[l]; }
+    T get(int i) { return query(i, i+1); }
+    T operator[](int i) { return query(i, i+1); }
+};
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    bool operator<(const ModInt &p) const { return x < p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+    // operator int() const { return x; }
+};
+using mint = ModInt<MOD>;
+
+////////// 最小費用流 //////////
+// --- ACL start --- //
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <vector>
+namespace atcoder {
+namespace internal {
+template <class E> struct csr {
+    std::vector<int> start;
+    std::vector<E> elist;
+    csr(int n, const std::vector<std::pair<int, E>>& edges)
+        : start(n + 1), elist(edges.size()) {
+        for (auto e : edges) {
+            start[e.first + 1]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            start[i] += start[i - 1];
+        }
+        auto counter = start;
+        for (auto e : edges) {
+            elist[counter[e.first]++] = e.second;
+        }
+    }
+};
+}  // namespace internal
+}  // namespace atcoder
+namespace atcoder {
+template <class Cap, class Cost> struct mcf_graph {
+  public:
+    mcf_graph() {}
+    mcf_graph(int n) : _n(n) {}
+    int add_edge(int from, int to, Cap cap, Cost cost) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        assert(0 <= cost);
+        int m = int(_edges.size());
+        _edges.push_back({from, to, cap, 0, cost});
+        return m;
+    }
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+        Cost cost;
+    };
+    edge get_edge(int i) {
+        int m = int(_edges.size());
+        assert(0 <= i && i < m);
+        return _edges[i];
+    }
+    std::vector<edge> edges() { return _edges; }
+    std::pair<Cap, Cost> flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    std::pair<Cap, Cost> flow(int s, int t, Cap flow_limit) {
+        return slope(s, t, flow_limit).back();
+    }
+    std::vector<std::pair<Cap, Cost>> slope(int s, int t) {
+        return slope(s, t, std::numeric_limits<Cap>::max());
+    }
+    std::vector<std::pair<Cap, Cost>> slope(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+        int m = int(_edges.size());
+        std::vector<int> edge_idx(m);
+        auto g = [&]() {
+            std::vector<int> degree(_n), redge_idx(m);
+            std::vector<std::pair<int, _edge>> elist;
+            elist.reserve(2 * m);
+            for (int i = 0; i < m; i++) {
+                auto e = _edges[i];
+                edge_idx[i] = degree[e.from]++;
+                redge_idx[i] = degree[e.to]++;
+                elist.push_back({e.from, {e.to, -1, e.cap - e.flow, e.cost}});
+                elist.push_back({e.to, {e.from, -1, e.flow, -e.cost}});
+            }
+            auto _g = internal::csr<_edge>(_n, elist);
+            for (int i = 0; i < m; i++) {
+                auto e = _edges[i];
+                edge_idx[i] += _g.start[e.from];
+                redge_idx[i] += _g.start[e.to];
+                _g.elist[edge_idx[i]].rev = redge_idx[i];
+                _g.elist[redge_idx[i]].rev = edge_idx[i];
+            }
+            return _g;
+        }();
+        auto result = slope(g, s, t, flow_limit);
+        for (int i = 0; i < m; i++) {
+            auto e = g.elist[edge_idx[i]];
+            _edges[i].flow = _edges[i].cap - e.cap;
+        }
+        return result;
+    }
+  private:
+    int _n;
+    std::vector<edge> _edges;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+        Cost cost;
+    };
+    std::vector<std::pair<Cap, Cost>> slope(internal::csr<_edge>& g,
+                                            int s,
+                                            int t,
+                                            Cap flow_limit) {
+        std::vector<std::pair<Cost, Cost>> dual_dist(_n);
+        std::vector<int> prev_e(_n);
+        std::vector<bool> vis(_n);
+        struct Q {
+            Cost key;
+            int to;
+            bool operator<(Q r) const { return key > r.key; }
+        };
+        std::vector<int> que_min;
+        std::vector<Q> que;
+        auto dual_ref = [&]() {
+            for (int i = 0; i < _n; i++) {
+                dual_dist[i].second = std::numeric_limits<Cost>::max();
+            }
+            std::fill(vis.begin(), vis.end(), false);
+            que_min.clear();
+            que.clear();
+            size_t heap_r = 0;
+            dual_dist[s].second = 0;
+            que_min.push_back(s);
+            while (!que_min.empty() || !que.empty()) {
+                int v;
+                if (!que_min.empty()) {
+                    v = que_min.back();
+                    que_min.pop_back();
+                } else {
+                    while (heap_r < que.size()) {
+                        heap_r++;
+                        std::push_heap(que.begin(), que.begin() + heap_r);
+                    }
+                    v = que.front().to;
+                    std::pop_heap(que.begin(), que.end());
+                    que.pop_back();
+                    heap_r--;
+                }
+                if (vis[v]) continue;
+                vis[v] = true;
+                if (v == t) break;
+                Cost dual_v = dual_dist[v].first, dist_v = dual_dist[v].second;
+                for (int i = g.start[v]; i < g.start[v + 1]; i++) {
+                    auto e = g.elist[i];
+                    if (!e.cap) continue;
+                    Cost cost = e.cost - dual_dist[e.to].first + dual_v;
+                    if (dual_dist[e.to].second - dist_v > cost) {
+                        Cost dist_to = dist_v + cost;
+                        dual_dist[e.to].second = dist_to;
+                        prev_e[e.to] = e.rev;
+                        if (dist_to == dist_v) {
+                            que_min.push_back(e.to);
+                        } else {
+                            que.push_back(Q{dist_to, e.to});
+                        }
+                    }
+                }
+            }
+            if (!vis[t]) {
+                return false;
+            }
+            for (int v = 0; v < _n; v++) {
+                if (!vis[v]) continue;
+                dual_dist[v].first -= dual_dist[t].second - dual_dist[v].second;
+            }
+            return true;
+        };
+        Cap flow = 0;
+        Cost cost = 0, prev_cost_per_flow = -1;
+        std::vector<std::pair<Cap, Cost>> result = {{Cap(0), Cost(0)}};
+        while (flow < flow_limit) {
+            if (!dual_ref()) break;
+            Cap c = flow_limit - flow;
+            for (int v = t; v != s; v = g.elist[prev_e[v]].to) {
+                c = std::min(c, g.elist[g.elist[prev_e[v]].rev].cap);
+            }
+            for (int v = t; v != s; v = g.elist[prev_e[v]].to) {
+                auto& e = g.elist[prev_e[v]];
+                e.cap += c;
+                g.elist[e.rev].cap -= c;
+            }
+            Cost d = -dual_dist[s].first;
+            flow += c;
+            cost += c * d;
+            if (prev_cost_per_flow == d) {
+                result.pop_back();
+            }
+            result.push_back({flow, cost});
+            prev_cost_per_flow = d;
+        }
+        return result;
+    }
+};
+}  // namespace atcoder
+// --- ACL end --- //
+////////// 最小費用流 //////////
+using namespace atcoder;
+
+void solve() {
+    map<char, ll> mp = {{'R', 0}, {'G', 1}, {'B', 2}};
+    ll N;
+    cin >> N;
+    vvl adj(3);
+    vector<ll> C(3);
+    rep(i, N*2) {
+        ll a;
+        char c;
+        cin >> a >> c;
+        adj[mp[c]].pb(a);
+        C[mp[c]]++;
+    }
+    rep(i, 3) sort(ALL(adj[i]));
+
+    vector<ll> odd;
+    rep(i, 3) {
+        if (C[i]%2 == 1) odd.pb(i);
+    }
+
+    if (odd.size() == 0) {
+        print(0);
+    } else {
+        ll N0 = adj[odd[0]].size();
+        ll N1 = adj[odd[1]].size();
+        ll even = 3-odd[0]-odd[1];
+        ll ans = INF;
+        // odd0:odd1
+        for (ll a : adj[odd[0]]) {
+            ll i = bisect_right(adj[odd[1]], a);
+            if (i) chmin(ans, abs(a-adj[odd[1]][i-1]));
+            if (i < N1) chmin(ans, abs(a-adj[odd[1]][i]));
+        }
+        // odd0:even, odd1:even
+        ll N2 = adj[even].size();
+        mcf_graph<ll, ll> mcf(N*2+4);
+        ll s = N*2;
+        ll t0 = N*2+1;
+        ll t1 = N*2+2;
+        ll g = N*2+3;
+        ll offset = 3;
+        mcf.add_edge(s, t0, 1, 0);
+        mcf.add_edge(s, t1, 1, 0);
+        rep(i, N0) {
+            mcf.add_edge(t0, i, 1, 0);
+            ll j = bisect_left(adj[even], adj[odd[0]][i]);
+            // 適当に近いとこ前後3ずつくらいだけつないでみる
+            ll l = max(j-offset, 0LL);
+            ll r = min(j+offset, N2);
+            rep(k, l, r) {
+                mcf.add_edge(i, N0+N1+k, 1, abs(adj[odd[0]][i]-adj[even][k]));
+            }
+        }
+        rep(i, N1) {
+            mcf.add_edge(t1, N0+i, 1, 0);
+            ll j = bisect_left(adj[even], adj[odd[1]][i]);
+            ll l = max(j-offset, 0LL);
+            ll r = min(j+offset, N2);
+            rep(k, l, r) {
+                mcf.add_edge(N0+i, N0+N1+k, 1, abs(adj[odd[1]][i]-adj[even][k]));
+            }
+        }
+        rep(i, N2) {
+            mcf.add_edge(N0+N1+i, g, 1, 0);
+        }
+        auto [f, c] = mcf.flow(s, g, 2);
+        if (f >= 2) chmin(ans, c);
+        print(ans);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
