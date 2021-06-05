4bd839b7974f6564f0cceab5b35c3654f20d1d9a
Coki <srcn4frdm.c@gmail.com>
2021-06-05 23:17:01 +0900

    participapted in cf1535

---
+++ CodeForces/cf1535_d.cpp
@@ -0,0 +1,379 @@
+/*
+・終了後20分くらいで自力AC。間に合わなかった。。
+・セグ木応用、二分木、2冪、クエリ系
+・セグ木でやるようなことを、セグ木の外でやる。
+　試合結果の文字列は、二分木の頂点と対応させることができて、
+　結果が更新されたら、自分の親側にだけ更新を反映させる。
+　ただし番号の対応付けは文字列をリバースさせた方が高さが合う。
+　これだと同じ高さ同士の左右も逆転してしまうが、
+　それは更新時の左辺と右辺の勝利関係の逆転させればOK。
+・終わってみればもっと早く詰めれそうに思えるのに、
+　実際はめちゃくちゃ時間かかった。ダメだなー、まだまだ。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
+#define name4(i, a, b, c, d, e, ...) e
+#define rep(...) name4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rep1(i, a) for(ll i = 0, _aa = a; i < _aa; i++)
+#define rep2(i, a, b) for(ll i = a, _bb = b; i < _bb; i++)
+#define rep3(i, a, b, c) for(ll i = a, _bb = b; (a <= i && i < _bb) or (a >= i && i > _bb); i += c)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define pb push_back
+#define mkp make_pair
+#define ALL(A) A.begin(), A.end()
+#define elif else if
+#define tostr to_string
+constexpr ll INF = 1e18;
+// constexpr ll INF = LONG_LONG_MAX;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr ld EPS = 1e-10;
+constexpr ld PI = M_PI;
+
+const string digits = "0123456789";
+const string ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
+const string ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string ascii_letters = ascii_lowercase + ascii_uppercase;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { return vector<vector<T>>(N, vector<T>(M, init)); }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { return vector<vector<vector<T>>>(N, vector<vector<T>>(M, vector<T>(L, init))); }
+template<typename T> vector<vector<vector<vector<T>>>> list4d(int N, int M, int L, int O, T init) { return vector<vector<vector<vector<T>>>>(N, vector<vector<vector<T>>>(M, vector<vector<T>>(L, vector<T>(O, init)))); }
+
+template<typename T=ll> vector<T> LIST(ll N) { vector<T> A(N); rep(i, 0, N) cin >> A[i]; return A; }
+
+void print() { cout << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
+template<typename T> void print(T out) { cout << out << '\n'; }
+template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
+template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(deque<T> A) { rep(i, 0, A.size()) { cout << A[i]; if (i != A.size()-1) cout << ' '; } cout << '\n'; }
+template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
+#define debug(x) (cout << #x << ": ", print(x));
+
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
+
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+template<typename T> pll divmod(ll a, T b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> &A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> &A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> &A) { return *min_element(ALL(A)); }
+
+ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
+
+ll pow(int x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, int n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
+
+int popcount(ll S) { return __builtin_popcountll(S); }
+int bit_length(ll x) { return x != 0 ? floor(log2(x))+1 : 0; }
+string bin(ll x) { string res; while (x) { if (x & 1) res += '1'; else res += '0'; x >>= 1; } reverse(ALL(res)); if(res == "") res += '0'; return res; }
+
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll x, ll y) { return (x * y) / gcd(x, y); }
+
+ld degrees(ld radians) { return radians * 180.0 / PI; }
+ld radians(ld degrees) { return degrees * PI / 180.0; }
+
+template<typename T> int bisect_left(vector<T> &A, T val) { return lower_bound(ALL(A), val) - A.begin(); }
+template<typename T> int bisect_right(vector<T> &A, T val) { return upper_bound(ALL(A), val) - A.begin(); }
+template<typename F> ll bisearch_min(ll mn, ll mx, const F &func) { ll ok = mx, ng = mn; while (ng+1 < ok) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+template<typename F> ll bisearch_max(ll mn, ll mx, const F &func) { ll ok = mn, ng = mx; while (ok+1 < ng) { ll mid = (ok+ng) / 2; if (func(mid)) ok = mid; else ng = mid; } return ok; }
+
+template<typename T> map<T, ll> Counter(vector<T> &A) { map<T, ll> res; for (T a : A) res[a]++; return res; }
+map<char, ll> Counter(string &S) { map<char, ll> res; for (char c : S) res[c]++; return res; }
+template<typename T1, typename T2> pair<vector<T1>, vector<T2>> zip(vector<pair<T1, T2>> &A) { ll N = A.size(); pair<vector<T1>, vector<T2>> res = {vector<T1>(N), vector<T2>(N)}; rep(i, N) { res.first[i] = A[i].first; res.second[i] = A[i].second; } return res; }
+
+template<typename T> struct Accumulate {
+    vector<T> acc; int N;
+    Accumulate(vector<T> &A) { N = A.size(); acc = A; rep(i, N-1) acc[i+1] += acc[i]; acc.insert(acc.begin(), 0); }
+    T query(int l, int r) { assert(0 <= l and l <= N and 0 <= r and r <= N); return acc[r]-acc[l]; }
+    T get(int i) { return query(i, i+1); }
+    T operator[](int i) { return query(i, i+1); }
+};
+
+template<int mod> struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+    ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+    ModInt &operator/=(const ModInt &p) { *this *= p.inverse(); return *this; }
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    bool operator<(const ModInt &p) const { return x < p.x; }
+    ModInt inverse() const { int a = x, b = mod, u = 1, v = 0, t; while(b > 0) { t = a / b; swap(a -= t * b, b); swap(u -= t * v, v); } return ModInt(u); }
+    ModInt pow(int64_t n) const { ModInt ret(1), mul(x); while(n > 0) { if(n & 1) ret *= mul; mul *= mul; n >>= 1; } return ret; }
+    friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, ModInt &a) { int64_t t; is >> t; a = ModInt< mod >(t); return (is); }
+    static int get_mod() { return mod; }
+    // operator int() const { return x; }
+};
+using mint = ModInt<MOD>;
+
+template<typename Monoid, typename F>
+struct SegmentTree {
+
+    int sz;
+    vector<Monoid> seg;
+
+    const F f;
+    const Monoid M1;
+
+    SegmentTree(int n, const F f, const Monoid &M1) : f(f), M1(M1) {
+        sz = 1;
+        while(sz < n) sz <<= 1;
+        seg.assign(2 * sz, M1);
+    }
+
+    SegmentTree(const F f, const Monoid &M1) : f(f), M1(M1) {}
+
+    void resize(int n) {
+        sz = 1;
+        while(sz < n) sz <<= 1;
+        seg.resize(2 * sz, M1);
+    }
+
+    void clear() {
+        seg.clear();
+    }
+
+    void set(int k, const Monoid &x) {
+        seg[k+sz] = x;
+    }
+
+    void build() {
+        for(int k = sz - 1; k > 0; k--) {
+            seg[k] = f(seg[2*k], seg[2*k+1]);
+        }
+    }
+
+    void build(vector<Monoid> &A) {
+        int n = A.size();
+        resize(n);
+        rep(i, 0, n) set(i, A[i]);
+        build();
+    }
+
+    void update(int k, const Monoid &x) {
+        k += sz;
+        seg[k] = x;
+        while(k >>= 1) {
+            seg[k] = f(seg[2*k], seg[2*k+1]);
+        }
+    }
+
+    Monoid query(int a, int b) {
+        Monoid L = M1, R = M1;
+        for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+            if(a & 1) L = f(L, seg[a++]);
+            if(b & 1) R = f(seg[--b], R);
+        }
+        return f(L, R);
+    }
+
+    Monoid operator[](const int &k) const {
+        return seg[k+sz];
+    }
+
+    Monoid all() {
+        return seg[1];
+    }
+
+    void print(int n) {
+        rep(i, n) {
+            cout << query(i, i+1);
+            if (i == n-1) cout << endl;
+            else cout << ' ';
+        }
+    }
+
+    template<typename C>
+    int find_subtree(int a, const C &check, Monoid &M, bool type) {
+        while(a < sz) {
+            Monoid nxt = type ? f(seg[2 * a + type], M) : f(M, seg[2 * a + type]);
+            if(check(nxt)) a = 2 * a + type;
+            else M = nxt, a = 2 * a + 1 - type;
+        }
+        return a - sz;
+    }
+
+    // 区間[a,N)でcheckの条件を満たすような最小位置を返す(なければ-1)
+    template<typename C>
+    int find_first(int a, const C &check) {
+        Monoid L = M1;
+        if(a <= 0) {
+            if(check(f(L, seg[1]))) return find_subtree(1, check, L, false);
+            return -1;
+        }
+        int b = sz;
+        for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+            if(a & 1) {
+                Monoid nxt = f(L, seg[a]);
+                if(check(nxt)) return find_subtree(a, check, L, false);
+                L = nxt;
+                ++a;
+            }
+        }
+        return -1;
+    }
+
+    // 区間[0,b)でcheckの条件を満たすような最大位置を返す(なければ-1)
+    template<typename C>
+    int find_last(int b, const C &check) {
+        Monoid R = M1;
+        if(b >= sz) {
+            if(check(f(seg[1], R))) return find_subtree(1, check, R, true);
+            return -1;
+        }
+        int a = sz;
+        for(b += sz; a < b; a >>= 1, b >>= 1) {
+            if(b & 1) {
+                Monoid nxt = f(seg[--b], R);
+                if(check(nxt)) return find_subtree(b, check, R, true);
+                R = nxt;
+            }
+        }
+        return -1;
+    }
+};
+
+template<typename Monoid, typename F>
+SegmentTree<Monoid, F> get_segment_tree(int N, const F& f, const Monoid& M1) {
+    return {N, f, M1};
+}
+
+template<typename Monoid, typename F>
+SegmentTree<Monoid, F> get_segment_tree(const F& f, const Monoid& M1) {
+    return {f, M1};
+}
+
+struct Node {
+
+    ll cnt;
+    char c;
+
+    Node(ll cnt, char c) : cnt(cnt), c(c) {};
+
+    Node() {};
+
+    operator ll() const { return cnt; }
+};
+
+const Node T = Node();
+
+void solve() {
+    ll K;
+    cin >> K;
+    string S;
+    cin >> S;
+    S.pb('*');
+    reverse(ALL(S));
+    ll N = S.size();
+
+    ll M = pow(2, K-1);
+
+    vector<Node> nodes;
+    ll sz = 1;
+    while (sz < M) sz <<= 1;
+    nodes.assign(2*sz, T);
+
+    rep(i, 1, 2*sz) {
+        if (S[i] == '?') nodes[i] = Node({2, S[i]});
+        else  nodes[i] = Node({1, S[i]});
+    }
+    for (int k=sz-1; k>0; k--) {
+        // nodes[k] = f(nodes[2*k], nodes[2*k+1]);
+        if (nodes[k].c == '?') {
+            nodes[k].cnt = nodes[2*k].cnt+nodes[2*k+1].cnt;
+        } elif (nodes[k].c == '0') {
+            nodes[k].cnt = nodes[2*k+1].cnt;
+        } else {
+            nodes[k].cnt = nodes[2*k].cnt;
+        }
+    }
+
+    ll Q;
+    cin >> Q;
+    rep(_, Q) {
+        ll k;
+        char c;
+        cin >> k >> c;
+        k = N-k;
+
+        nodes[k].c = c;
+        while (k) {
+            // nodes[k] = f(nodes[2*k], nodes[2*k+1]);
+            if (k >= sz) {
+                if (nodes[k].c == '?') {
+                    nodes[k].cnt = 2;
+                } elif (nodes[k].c == '0') {
+                    nodes[k].cnt = 1;
+                } else {
+                    nodes[k].cnt = 1;
+                }
+            } else {
+                if (nodes[k].c == '?') {
+                    nodes[k].cnt = nodes[2*k].cnt+nodes[2*k+1].cnt;
+                } elif (nodes[k].c == '0') {
+                    nodes[k].cnt = nodes[2*k+1].cnt;
+                } else {
+                    nodes[k].cnt = nodes[2*k].cnt;
+                }
+            }
+            k >>= 1;
+        }
+        ll res = nodes[1].cnt;
+        print(res);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
