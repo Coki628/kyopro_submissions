5385f450b42c0d2b76938066277c151cfe223b3f
Coki <srcn4frdm.c@gmail.com>
2021-12-27 18:44:30 +0900

    renamed oj-configs directory

--- cf.bat
+++ cf.bat
@@ -9,7 +9,7 @@ cd CodeForces
 @REM 全問題取得
 if %2 == "" (
     @REM 全問題を取得
-    call oj-prepare --config-file %project_root%\_lib\cpp\_oj-configs\prepare.config.cf.toml https://codeforces.com/contest/%1
+    call oj-prepare --config-file %project_root%\_lib\cpp\oj-configs\prepare.config.cf.toml https://codeforces.com/contest/%1
     @REM A問題を開く
     call code %1\A\1.cpp
     cd ..\..
@@ -18,7 +18,7 @@ if %2 == "" (
     @REM なければコンテストの階層を作成
     if not exist %1 mkdir %1
     cd %1
-    call oj-prepare --config-file %project_root%\_lib\cpp\_oj-configs\prepare.config.cf.toml https://codeforces.com/contest/%1/problem/%2
+    call oj-prepare --config-file %project_root%\_lib\cpp\oj-configs\prepare.config.cf.toml https://codeforces.com/contest/%1/problem/%2
     @REM 問題を開く
     call code %2\1.cpp
     cd ..\..
--- chef.bat
+++ chef.bat
@@ -9,7 +9,7 @@ cd codechef
 @REM 全問題取得
 if %2 == "" (
     @REM 全問題を取得
-    call oj-prepare --config-file %project_root%\_lib\cpp\_oj-configs\prepare.config.cf.toml https://www.codechef.com/contest/%1
+    call oj-prepare --config-file %project_root%\_lib\cpp\oj-configs\prepare.config.cf.toml https://www.codechef.com/contest/%1
     @REM 問題Aを開きたいけど問題IDが通し番号じゃないので分からない
     cd ..
 @REM 問題指定
@@ -17,7 +17,7 @@ if %2 == "" (
     @REM なければコンテストの階層を作成
     if not exist %1 mkdir %1
     cd %1
-    call oj-prepare --config-file %project_root%\_lib\cpp\_oj-configs\prepare.config.cf.toml https://www.codechef.com/%1/problems/%2
+    call oj-prepare --config-file %project_root%\_lib\cpp\oj-configs\prepare.config.cf.toml https://www.codechef.com/%1/problems/%2
     @REM 問題を開く
     call code %2\1.cpp
     cd ..\..
