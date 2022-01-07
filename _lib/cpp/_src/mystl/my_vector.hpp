c021e81704ed9a7138228ce53ba90885d8644849
Coki <srcn4frdm.c@gmail.com>
2022-01-07 19:07:41 +0900

    fixed my_vector

--- _lib/cpp/_src/mystl/my_vector.hpp
+++ _lib/cpp/_src/mystl/my_vector.hpp
@@ -1,12 +1,19 @@
 #include "../base.hpp"
+#include "../common/print.hpp"
 
-// ※なんかコンストラクタが言うこと聞かないので微妙。。
-template<typename _Tp, typename _Alloc=std::allocator<_Tp>>
-struct my_vector : public vector<_Tp, _Alloc> {
+template<typename _Tp>
+struct my_vector : vector<_Tp> {
+    // コンストラクタの継承
+    using vector<_Tp>::vector;
     _Tp pop() {
         auto res = this->back();
         this->pop_back();
         return res;
     }
 };
-#define vector my_vector
+
+template<typename T>
+void print(const my_vector<T> &A) {
+    vector<T> V(A.begin(), A.end());
+    print(V);
+}
