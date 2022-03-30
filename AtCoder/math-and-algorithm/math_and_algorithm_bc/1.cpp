347c3f5c3a06053fa637c166b6d19261c365c562
Coki <srcn4frdm.c@gmail.com>
2022-03-30 22:27:36 +0900

    E8さん本5-3：偶奇

---
+++ AtCoder/math-and-algorithm/math_and_algorithm_bc/1.cpp
@@ -0,0 +1,53 @@
+/*
+・偶奇
+・これはいけたと思う。多分。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define CONSTANTS
+// #define CAST_MINT_TO_LL
+#include "base.hpp"
+
+constexpr long long INF = 1e18;
+// constexpr long long INF = LONG_LONG_MAX;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr long double EPS = 1e-10;
+constexpr long double PI = M_PI;
+
+#include "macros.hpp"
+#include "combinatorics/ModInt.hpp"
+#include "template.hpp"
+
+void solve() {
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N);
+    cin >> A;
+
+    ll sm = sum(A);
+    if (sm%2 == K%2 and sm <= K) {
+        Yes();
+    } else {
+        No();
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
