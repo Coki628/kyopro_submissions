7f65bbe9797ba3f99fdd44e5f4e92ae252c5326b
Coki <srcn4frdm.c@gmail.com>
2022-05-09 11:02:04 +0900

    updated scripts

--- ac.py
+++ ac.py
@@ -1,9 +1,13 @@
 # 実行方法
+# C++で生成
 # python ac.py 問題URL
+# pythonで生成
+# python ac.py 問題URL -p
 
 import sys
 import os
 from subprocess import run
+import shutil
 
 args = sys.argv
 
@@ -17,9 +21,25 @@ project_root = os.getcwd()
 os.chdir('{0}/AtCoder'.format(os.getcwd()))
 os.makedirs(contest_name, exist_ok=True)
 os.chdir('{0}/{1}'.format(os.getcwd(), contest_name))
-run(
-    'oj-prepare --config-file {0}/_tools/oj-configs/prepare.config.ac.toml {1}'.format(project_root, url),
-    shell=True, encoding='utf-8'
-)
+os.makedirs(problem_name, exist_ok=True)
 os.chdir('{0}/{1}'.format(os.getcwd(), problem_name))
-run('code 1.cpp', shell=True, encoding='utf-8')
+if not os.path.exists('test'):
+    run('oj download {0}'.format(url), shell=True, encoding='utf-8')
+if len(args) >= 3 and args[2] == '-p':
+    if os.path.exists('1.py'):
+        print('1.py already exists')
+    else:
+        shutil.copy(
+            '{0}/_tools/pythonlib/tpl.py'.format(project_root),
+            '{0}/1.py'.format(os.getcwd()),
+        )
+    run('code 1.py', shell=True, encoding='utf-8')
+else:
+    if os.path.exists('1.cpp'):
+        print('1.ccp already exists')
+    else:
+        shutil.copy(
+            '{0}/_tools/oj-configs/tpl.cpp'.format(project_root),
+            '{0}/1.cpp'.format(os.getcwd()),
+        )
+    run('code 1.cpp', shell=True, encoding='utf-8')
--- yuki.py
+++ yuki.py
@@ -4,6 +4,7 @@
 import sys
 import os
 from subprocess import run
+import shutil
 
 args = sys.argv
 
@@ -14,9 +15,15 @@ problem_name = url.split('/')[-1]
 project_root = os.getcwd()
 
 os.chdir('{0}/yukicoder'.format(os.getcwd()))
-run(
-    'oj-prepare --config-file {0}/_tools/oj-configs/prepare.config.ac.toml {1}'.format(project_root, url),
-    shell=True, encoding='utf-8'
-)
+os.makedirs(problem_name, exist_ok=True)
 os.chdir('{0}/{1}'.format(os.getcwd(), problem_name))
+if not os.path.exists('test'):
+    run('oj download {0}'.format(url), shell=True, encoding='utf-8')
+if os.path.exists('1.cpp'):
+    print('1.ccp already exists')
+else:
+    shutil.copy(
+        '{0}/_tools/oj-configs/tpl.cpp'.format(project_root),
+        '{0}/1.cpp'.format(os.getcwd()),
+    )
 run('code 1.cpp', shell=True, encoding='utf-8')
