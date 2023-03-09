f7492596619dae22a76e1e72b1a971c320a6b12e
Coki <srcn4frdm.c@gmail.com>
2023-03-09 12:48:24 +0900

    upsolved abc283_f

---
+++ AtCoder/abc283/abc283_f/2.cpp
@@ -0,0 +1,104 @@
+/*
+・やっとupsolveした。
+・45度回転、座圧、WM、range_freq、二分探索
+・(i,P[i])=(x,y)として座標平面に帰着するとabc233_hと同じ感じにできる。
+　でも20万にlog3つさすがにTLつらくてこれはTLE。。
+・下処理して座圧のlog外した。下駄履かせればいいんだけど、
+　どこまで作ればいいか結構悩んだし、使う時もはみ出すから調整が必要だった。
+・これでAC2.0秒ギリギリ。
+*/
+
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+#define CONSTANTS
+#define __LOCAL
+// #define CAST_MINT_TO_LL
+#include "base.hpp"
+
+constexpr long long INF = 1e18;
+// constexpr long long INF = LONG_LONG_MAX;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr long double EPS = 1e-10;
+constexpr long double PI = M_PI;
+
+#include "macros.hpp"
+#include "combinatorics/ModInt.hpp"
+using mint = ModInt<MOD>;
+#include "template.hpp"
+
+#include "datastructure/WaveletMatrix.hpp"
+
+void solve() {
+    ll N;
+    cin >> N;
+    vector<int> P(N);
+    cin >> P;
+    P--;
+
+    vector<int> U, V;
+    vector<pii> cptmp;
+    rep(i, N) {
+        int u = i - P[i];
+        int v = i + P[i];
+        U.eb(u);
+        V.eb(v);
+        cptmp.eb(u, v);
+    }
+
+    cptmp.eb(MOD, MOD);
+    V.eb(MOD);
+    Compress<pii> cpu(cptmp);
+    Compress<int> cpv(V);
+    int W = cpu.size();
+    int H = cpv.size();
+    // vの情報を含めて一意にしたu座標をキー、v座標を値としてWMを構成する
+    vector<int> wmtmp(W, MOD);
+    rep(i, N) {
+        wmtmp[cpu[{U[i], V[i]}]] = cpv[V[i]];
+    }
+    WaveletMatrix<int> wm(wmtmp);
+
+    // 下処理で座圧のlogを外す
+    vector<int> mpu(800007), mpv(800007);
+    int offset = 400000;
+    rep(i, -offset, 400007) {
+        mpu[i + offset] = cpu[{i, -MOD}];
+        mpv[i + offset] = cpv[i];
+    }
+
+    vector<int> ans(N);
+    rep(i, N) {
+        int u = i - P[i];
+        int v = i + P[i];
+        ans[i] = bisearch_min(-1, 400000, [&](int m) {
+            // (u,v)を中心とした1辺が2mの正方形領域の中にある点の個数
+            int lu = max(u - m + offset, 0);
+            int ru = min(u + m + 1 + offset, 800006);
+            int lv = max(v - m + offset, 0);
+            int rv = min(v + m + 1 + offset, 800006);
+            int cnt = wm.range_freq(mpu[lu], mpu[ru], mpv[lv], mpv[rv]);
+            // 2個以上ある最小値なら自身以外への最短距離が取れる
+            return cnt >= 2;
+        });
+    }
+    print(ans);
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+
+    // single test case
+    solve();
+
+    // multi test cases
+    // int T;
+    // cin >> T;
+    // while (T--) solve();
+
+    return 0;
+}
